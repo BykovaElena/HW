d850d319c6916c0cc603fa3d10b504258bbe3037
Elena <Galushk1na@yandex.ru>
2022-04-16 10:02:49 +0300

    new

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
 
-import React from "react"; 
+import React, { useState, useEffect } from "react"
 import { Layout } from 'antd';
 const { Header, Footer,  Content } = Layout;
 import { _Card } from "../components/PostList";
 import { Info } from "../components/Content";
+import Api from '../utils/Api.js';
+
+
+
+
+
 
 export const App = () => {
+    useEffect(() => {
+        Api.getPostList()
+            .then(data => console.log(data))
+            .catch(err => console.log(err))
+    })
     return (
         <>
             <Layout>
---
+++ utils/Api.js
@@ -0,0 +1,23 @@
+class Api {
+    constructor({ baseUrl, token }) {
+        this._baseUrl = baseUrl;
+        this._token = `Bearer${token}`;
+    }
+    getPostList() {
+        return fetch(`${this._baseUrl}/posts`, {
+            headers: {
+                authorization: this._token,
+            }
+        }).then((res) => {
+            return res.ok ? res.json() : Promise.reject(`Ошибка: ${res.status}`)
+        })
+    }
+}
+
+const config = {
+    baseUrl: 'https://api.react-learning.ru',
+    token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MjJiNmZmYzA5YjEyZjgwZjRjMTBiYjAiLCJpYXQiOjE2NDcwMTM4ODUsImV4cCI6MTY3ODU0OTg4NX0.JuPhEHkPxf0RNoN9tAB2oH27rizPL3_8TWGFvbXD4zM',
+}
+
+const api = new Api(config);
+export default api;
